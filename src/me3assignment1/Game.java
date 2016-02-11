b0330dec21bac5b109edfb966006a956bde8564d
Patrick Duncan <patrick.duncan@outlook.com>
2016-02-10 21:17:14 -0500

    Disc placement

--- src/me3assignment1/Game.java
+++ src/me3assignment1/Game.java
@@ -6,25 +6,30 @@ import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.TimerTask;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
+import javax.swing.JLayeredPane;
 
 public class Game {
 
-    private static final int FRAME_WIDTH = 900, FRAME_HEIGHT = 550;
-    private static JFrame frame;
-    private static JPanel layoutP, buttonP, colourP, redP, blueP, boardP;
-    private static JLabel redL, blueL;
-    private static JButton emptyB, modifyB;
-    private static Press press;
-    private static Click click;
-    private static boolean modifying = false;
-    private static enum e {none, blue, red};
-    private static final e[] states = new e[]{e.none, e.blue, e.red};
-    private static final int[][] points = new int[16][2];
+    private final int FRAME_WIDTH = 900, FRAME_HEIGHT = 550;
+    private JFrame frame;
+    private JPanel layoutP, buttonP, colourP, redP, blueP, boardP;
+    private JLayeredPane discLayer;
+    private JLabel redL, blueL;
+    private JButton emptyB, modifyB, redB, blueB;
+    private Press press;
+    private Click click;
+    private boolean modifying = false, redTurn = true;
+    private enum e {none, blue, red};
+    private final e[] states = new e[]{e.none, e.blue, e.red};
+    private final int[][] points = new int[16][2];
+    private JLabel[] discs = new JLabel[16];
+    private ImageIcon redImg, blueImg;
     
     /* Setup the frame and panels
     */
@@ -34,25 +39,31 @@ public class Game {
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setResizable(false);
         frame.setTitle("Six Men's Morris");
+        frame.setLayout(new BorderLayout());
         // Main layout panel setup
-        layoutP = new JPanel();
-        layoutP.setLayout(new BorderLayout());
+        layoutP = new JPanel(new BorderLayout());
+        layoutP.setBounds(0, 0, FRAME_WIDTH, FRAME_HEIGHT);
         // Red layoutP setup
-        redP = new JPanel();
+        redP = new JPanel(new GridLayout(2, 1, 0, 0));
         redL = new JLabel("   Red Score: 0   ");
         redP.add(redL);
+        redB = new JButton("Place Red Discs");
+        redP.add(redB);
+        redB.setVisible(false);
         redP.setBackground(Color.red);
         // Blue layoutP setup
-        blueP = new JPanel();
+        blueP = new JPanel(new GridLayout(2, 1, 0, 0));
         blueL = new JLabel("   Blue Score: 0   ");
         blueP.add(blueL);
+        blueB = new JButton("Place Blue Discs");
+        blueB.setVisible(false);
+        blueP.add(blueB);
         blueP.setBackground(Color.blue);
         // Board layoutP setup
         boardP = new JPanel();
         boardP.setBackground(Color.WHITE);
         ImageIcon board = createImageIcon("/board.png");
-        JLabel boardLabel = new JLabel(board);     // Must add imageicon to label
-        boardP.add(boardLabel);
+        boardP.add(new JLabel(board));
         // Colour layoutP setup
         colourP = new JPanel();
         colourP.setLayout(new GridLayout(2, 1, 0, 0));
@@ -72,35 +83,47 @@ public class Game {
         layoutP.add(colourP, BorderLayout.WEST);
         layoutP.add(boardP, BorderLayout.CENTER);
         layoutP.add(buttonP, BorderLayout.EAST);
-        frame.add(layoutP);
+        // Setup for the layeredpane which will hold all of the discs
+        discLayer = new JLayeredPane();
+        frame.add(discLayer);
+        discLayer.setBounds(0, 0, FRAME_WIDTH, FRAME_HEIGHT);
+        discLayer.add(layoutP, new Integer(0));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        pointSetUp();
+        discSetUp();
     }
     
     /*
     */
-    public static void pointSetUp() {
+    private void pointSetUp() {
         points[0] = new int[]{248, 78};
         points[1] = new int[]{450, 78};
-        points[2] = new int[]{657, 78};
+        points[2] = new int[]{653, 78};
         points[3] = new int[]{348, 180};
-        points[4] = new int[]{455, 180};
-        points[5] = new int[]{557, 180};
+        points[4] = new int[]{450, 180};
+        points[5] = new int[]{555, 180};
         points[6] = new int[]{250, 280};
         points[7] = new int[]{350, 280};
         points[8] = new int[]{555, 280};
-        points[9] = new int[]{657, 280};
-        points[10] = new int[]{350, 280};
-        points[11] = new int[]{455, 385};
+        points[9] = new int[]{653, 280};
+        points[10] = new int[]{350, 385};
+        points[11] = new int[]{450, 385};
         points[12] = new int[]{555, 385};
         points[13] = new int[]{250, 487};
-        points[14] = new int[]{452, 487};
-        points[15] = new int[]{660, 487};
+        points[14] = new int[]{450, 487};
+        points[15] = new int[]{653, 487};
     }
+    
+    private void discSetUp() {
+        redImg = createImageIcon("/red.png");
+        blueImg = createImageIcon("/blue.png");
+    }
+    
     
     /*
     */
-    public static void start() {
+    public  void start() {
         press = new Press();
         emptyB.addActionListener(press);
         modifyB.addActionListener(press);
@@ -123,7 +146,7 @@ public class Game {
 
     /* Action listener for the buttons, when a button is pressed the actionPerformed will be invoked 
     */
-    private static class Press implements ActionListener {
+    private  class Press implements ActionListener {
 
         @Override
         public void actionPerformed(java.awt.event.ActionEvent ae) {
@@ -131,38 +154,78 @@ public class Game {
                 modifying = true;
                 emptyB.setText("Done");
                 modifyB.setText("Restart Edit");                
+                redB.addActionListener(press);
+                redB.setVisible(true);
+                blueB.setVisible(true);
+                blueB.addActionListener(press);
            }
            else if (modifyB.isFocusOwner() && modifying) {
                //restart, remove discs
            }
            else if (emptyB.isFocusOwner() && modifying) {
-               //CHeck if shit's good
+               //Check if shit's good
            }
+           else if (redB.isFocusOwner() && modifying) {
+               redTurn = true;
+           } 
+           else if (blueB.isFocusOwner() && modifying) {
+               redTurn = false;
+           }           
         }
     }
     
-    private static class Click implements MouseListener {
-
+    private boolean pressed = false;
+    private long pressTime = 0;
+    private int index = 0;
+    private class Click implements MouseListener {
+        
         @Override
         public void mouseClicked(MouseEvent me) {}   // Pressed and releaseed
 
         @Override
         public void mousePressed(MouseEvent me) {   // Just the download motion
-            int x = me.getX(), y = me.getY();
-            boolean canPlace = false;
-            for (int[] point : points) {
-                if (Math.abs(point[0] - x) < 25 && Math.abs(point[1] - y) < 25) {
-                    canPlace = true;
-                    break;
+            if (modifying) {     
+                pressed = true;
+                pressTime = System.currentTimeMillis();
+                index = 0;
+                int x = me.getX(), y = me.getY(), placeX = 0, placeY = 0;
+                boolean canPlace = false;
+                for (int[] point : points) {
+                    if (Math.abs(point[0] - x) < 25 && Math.abs(point[1] - y) < 25) {
+                        canPlace = true;
+                        placeX = point[0];
+                        placeY = point[1];
+                        break;
+                    }
+                    index++;
                 }
-            }
-            if (canPlace) {
-                System.out.println("place");
+                if (canPlace) {
+                    System.out.println("presssed " + placeX + " " + placeY);
+                    //JLabel img;
+                    if (redTurn) {
+                        discs[index] = new JLabel(redImg);
+                        discs[index].setBounds(placeX-11, placeY-53, 50, 50);
+                    } else {
+                        discs[index] = new JLabel(blueImg);
+                        discs[index].setBounds(placeX-11, placeY-53, 50, 50);
+                    }
+                    discLayer.add(discs[index], new Integer(1));
+                }
             }
         }
 
         @Override
-        public void mouseReleased(MouseEvent me) {}
+        public void mouseReleased(MouseEvent me) {
+            if (modifying && System.currentTimeMillis() - pressTime > 1000f) {
+               pressed = false;
+               System.out.println(index);
+               System.out.println((discs[index].getClass().getName()));
+               //discLayer.remove(discs[index]);
+               discLayer.remove(0);
+               discLayer.repaint();
+               System.out.println("held"); //discLayer.remove
+            }
+        }
 
         @Override
         public void mouseEntered(MouseEvent me) {}
--- src/me3assignment1/ME3Assignment1.java
+++ src/me3assignment1/ME3Assignment1.java
@@ -3,8 +3,8 @@ package me3assignment1;
 public class ME3Assignment1 {
     
     public static void main(String[] args) {
-        new Game().setUp();
-        Game.pointSetUp();
-        Game.start();   
+        Game game = new Game();
+        game.setUp();
+        game.start();   
     }    
 }
